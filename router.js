f6d528f8766fae8e217422b24f63b8d2c57b58d6
tmonk <felixtypingmonkey@gmail.com>
2019-06-29 23:43:53 +0800

    Using token as webhook pathname to make sure request is come from Telegram API.

--- router.js
+++ router.js
@@ -1,6 +1,7 @@
 function router(response, pathname, postData, handler, copLogic, config) {
     //console.log("About to route a request for " + pathname);
-    if (pathname === "/") {
+    if (pathname === "/"+config.telegramToken) {
+        // using token as pathname to make sure request is send by Telegram API.
         handler(response, postData, copLogic, config);
     } else {
         console.log("No request handler found for " + pathname);
